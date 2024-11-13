430ccf7e1e5c7b6a9c1a4351d67d97e915db34f1
Laíza Barreto Píres <152499386+UserlalaAluna@users.noreply.github.com>
2024-11-13 09:13:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Inicio de uma função 
 function toggleMenu() {
     // acessos os elementos a ser modificados
     const navLinks = document.querySelector('.nav-links')
@@ -38,6 +40,8 @@ window.addEventListener('click', (event) => {
   }
 
 })
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function(e) {
         e.preventDefault();
@@ -50,4 +54,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+// logica para ativar após clicar em serviços
+document.querySelectorAll('.servico-card').forEach(div => {
+
+    div.addEventListener('click', () => {
+
+         const aparecer = document.getElementById(div.dataset.aparecer)
+
+         aparecer.style.display = 'flex'
+    })
+
+})
+document.querySelectorAll('.aparecer').forEach(div => {
+
+  div.addEventListener('click', () => {
+      div.closest('.aparecer').style.display = 'none'
+  })
+})
+
+window.addEventListener('click', (event) => {
+ 
+  if (event.target.classList.contains('aparecer')) {
+      event.target.style.display = 'none'
+  }
+
+})
 
